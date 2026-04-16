ee2901bbc69107677232e27298256b1b70d02623
mateogeffroy <mateogeffroy@gmail.com>
2026-04-16 17:08:14 -0300

    Fix de lo anterior

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -83,7 +83,7 @@ export default function AuthPage() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback` 
+          redirectTo: `${window.location.origin}/` 
         }
       });
       if (error) throw error;
